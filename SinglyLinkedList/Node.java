6c31fc504b44791390101b9d37a6d17e16381e7c
Kedar Kamthe <kedarkamthe2006@gmail.com>
2019-06-04 15:55:46 +0530

    Create Node.java

---
+++ SinglyLinkedList/Node.java
@@ -0,0 +1,33 @@
+public class Node<T> {
+    T data;
+    Node<T> next;
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
+}
